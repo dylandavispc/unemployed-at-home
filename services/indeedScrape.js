f30083424f9ccf04ca716dc621014317e6b1c0cb
Dylan Davis <dylandavispc@gmail.com>
2020-03-28 16:49:17 -0400

    Indeed scraping with link and title

--- services/indeedScrape.js
+++ services/indeedScrape.js
@@ -3,40 +3,35 @@ const cheerio = require('cheerio');
 
 module.exports = async ( req, res ) => {
 
-    // Test Variables
-    let searchTerm = "full+stack+web+developer"
-    let location = "Orlando%2C+FL"
+	// const { searchTerm, location } = req.query;
 
-    // const { searchTerm, location }
+  // Test Variables
+  let searchTerm = "full+stack+web+developer"
+  let location = "Orlando%2C+FL"
 
-    axios.get("https://www.indeed.com/jobs?q=full+stack+web+developer&l=Orlando%2C+FL").then(function(response) {
+  const url = "https://www.indeed.com/jobs?q=" + searchTerm + "r&l=" + location;
 
-  // Load the HTML into cheerio
-  var $ = cheerio.load(response.data);
+	try {
+		axios.get(url).then(function(response) {
+			var $ = cheerio.load(response.data);
 
-  // Make an empty array for saving our scraped info
-  var results = [];
+			var results = [];
+		
+			$("div.result").each(function(i, element) {
 
-  // With cheerio, look at each award-winning site, enclosed in "figure" tags with the class name "site"
-  $("div.result").each(function(i, element) {
+				var preLink = $(element).find("a").attr("href");
+				const link = "https://www.indeed.com" + preLink;
 
-    /* Cheerio's find method will "find" the first matching child element in a parent.
-     *    We start at the current element, then "find" its first child a-tag.
-     *    Then, we "find" the lone child img-tag in that a-tag.
-     *    Then, .attr grabs the imgs srcset value.
-     *    The srcset value is used instead of src in this case because of how they're displaying the images
-     *    Visit the website and inspect the DOM if there's any confusion
-    */
-    var preLink = $(element).find("a").attr("href");
+				const title = $(element).find("div.title").text().trim();
 
-    const postLink = "https://www.indeed.com" + preLink;
+				results.push({ link, title });
+			});
 
-    // Push the image's URL (saved to the imgLink var) into the results array
-    results.push({ link: postLink });
-  });
+		res.json(results)
 
-  // After looping through each element found, log the results to the console
-  res.json(results)
-  console.log(results);
-});
+		});
+
+	} catch(err) {
+		console.log(err);
+	};
 };

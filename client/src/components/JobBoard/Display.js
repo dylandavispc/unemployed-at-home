c9016f1c8147c028ff899ed734124ebc986bc455
Dylan Davis <dylandavispc@gmail.com>
2020-04-03 14:31:17 -0400

    Managed to get over react-hook probles... jobs now appending to display board

--- client/src/components/JobBoard/Display.js
+++ client/src/components/JobBoard/Display.js
@@ -10,15 +10,40 @@ export default function Display() {
         searchJobs();
     }, []);
 
-    const searchJobs = () => {
-        //Scrape Indeed
-        axios.get("http://localhost:3001/api/scrape/indeed")
-            .then(res => console.log(res))
+const indeedList = indeedJobs.map(job => <li>{job.title}</li>)
+
+    const searchJobs = async () => {
+        // Scrape Indeed
+        // let indeedRes = await axios.get("http://localhost:3001/api/scrape/indeed");
+        // let indeed = indeedRes.data;
+        //     // Console log results
+        // setIndeed(indeedRes.data)
+        // console.log(indeed)
+        // console.log(indeedJobs)
+
+        axios.get('http://localhost:3001/api/scrape/indeed')
+        .then(function (response) {
+            console.log(response.status);
+
+            if (response.status === 200)
+                setIndeed(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+
+            // Fill Table with Results
+            // .then(res => {
+            //     res.map()
+            // })
     }
 
     return (
         <div>
-            
+            <ul>
+                {indeedList}
+            </ul>
         </div>
     )
 }
--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const morganBody = require('morgan-body');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
